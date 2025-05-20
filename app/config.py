fb73c0d3667289e7ac26cbb906878a8e1b1feae4
Murilo Souza Almeida <murilo.familia.sa@gmail.com>
2025-05-20 17:15:03 -0300

    adição do valor padrão de 30 minutos para a variável de ambiente POC_EXPIRATION_TOKEN_TIME

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from app import BaseSettings, Options
 
 class Settings(BaseSettings):
     poc_secret_key_token : str
-    poc_expiration_token_time: int
+    poc_expiration_token_time: int = 30
     poc_algorithm_jwt_token: str = "HS256"
     poc_selenium_headless: str = "S"
 
